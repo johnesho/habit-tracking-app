073bf09348a47be1e5db5e310f11da164ec7c0a4
johnesho <94701175+johnesho@users.noreply.github.com>
2023-05-04 15:43:00 +0100

    Update analytics.py

--- app/analytics.py
+++ app/analytics.py
@@ -75,9 +75,66 @@ class Analytics:
                     formatted_line += line
                     formatted_line += "-".center(length, "-")+"\n"
                 print(formatted_line)
-    
+
     @staticmethod
-    def show_best_streak_habit():
+    def show_same_periodicity_habit():
+        """
+        Prints the same periodicity habits in a tabular form.
+        """
+        if os.path.exists("habit_tracker_database.txt"):
+            habit_name = ""
+            habit_description = ""
+            habit_periodicity = ""
+            habit_times_completed = ""
+            habit_last_completed = ""
+            habit_current_streak = ""
+            habit_best_streak = ""
+            highest_name_length = 0
+            highest_description_length = 0
+            highest_periodicity_length = 11
+            highest_times_completed_length = 15
+            highest_last_completed_length = 14
+            highest_current_streak_length = 14
+            highest_best_streak_length = 11
+            formatted_line = ""
+            best_streak = 0
+            with open("habit_tracker_database.txt", "r") as f:
+                for line in f:
+                    if best_streak <= int(line.strip().split(',')[5]):
+                        best_streak = int(line.strip().split(",")[6])
+                        habit_name = line.strip().split(',')[0]
+                        highest_name_length = len(habit_name)
+                        habit_description = line.strip().split(',')[1]
+                        highest_description_length = len(habit_description)
+                        habit_periodicity = line.strip().split(',')[2]
+                        if habit_periodicity == str(Habit.DAILY):
+                            habit_periodicity = "DAILY"
+                        elif habit_periodicity == str(Habit.WEEKLY):
+                            habit_periodicity = "WEEKLY"
+                        elif habit_periodicity == str(Habit.MONTHLY):
+                            habit_periodicity = "MONTHLY"
+                        else:
+                            habit_periodicity = "Every "+str(habit_periodicity)+" day"
+                        habit_times_completed = line.strip().split(',')[3]
+                        habit_last_completed = line.strip().split(',')[4]
+                        habit_last_completed = "None" if habit_last_completed == "" else habit_last_completed
+                        habit_current_streak = line.strip().split(',')[5]
+                        habit_best_streak = line.strip().split(',')[6]
+                length = 2+highest_name_length+3+highest_description_length+3+highest_periodicity_length+3+highest_times_completed_length+3+highest_last_completed_length+3+highest_current_streak_length+3+highest_best_streak_length+3
+                formatted_line = "-".center(length, "-")+"\n"
+                formatted_line += "| "+("NAME".center(highest_name_length, " "))
+                formatted_line += " | "+("DESCRIPTION".center(highest_description_length, " "))
+                formatted_line += " | "+("PERIODICITY".center(highest_periodicity_length, " "))
+                formatted_line += " | TIMES COMPLETED"
+                formatted_line += " | "+("LAST COMPLETED".center(highest_last_completed_length, " "))
+                formatted_line += " | CURRENT STREAK | BEST STREAK |\n"
+                formatted_line += "-".center(length, "-")+"\n"
+                formatted_line += "|"+(habit_name.center(highest_name_length+2, " "))+"| "+(habit_description.center(highest_description_length, " "))+" | "+(habit_periodicity.center(highest_periodicity_length, " "))+" | "+(habit_times_completed.center(highest_times_completed_length, " "))+" | "+(habit_last_completed.center(highest_last_completed_length, " "))+" | "+(habit_current_streak.center(highest_current_streak_length, " "))+" | "+(habit_best_streak.center(highest_best_streak_length, " "))+" |\n"
+                formatted_line += "-".center(length, "-")+"\n"
+                print(formatted_line)
+                    
+    @staticmethod
+    def show_longest_streak_habit():
         """
         Prints the last habit that has the best steak in a tabular form.
         """
@@ -133,10 +190,11 @@ class Analytics:
                 formatted_line += "-".center(length, "-")+"\n"
                 print(formatted_line)
 
+
     @staticmethod
-    def show_most_completed_habit():
+    def show_given_habit():
         """
-        Prints the last most completed habit in a tabular form.
+        Prints the given habit in a tabular form.
         """
         if os.path.exists("habit_tracker_database.txt"):
             habit_name = ""
@@ -188,4 +246,4 @@ class Analytics:
                 formatted_line += "-".center(length, "-")+"\n"
                 formatted_line += "|"+(habit_name.center(highest_name_length+2, " "))+"| "+(habit_description.center(highest_description_length, " "))+" | "+(habit_periodicity.center(highest_periodicity_length, " "))+" | "+(habit_times_completed.center(highest_times_completed_length, " "))+" | "+(habit_last_completed.center(highest_last_completed_length, " "))+" | "+(habit_current_streak.center(highest_current_streak_length, " "))+" | "+(habit_best_streak.center(highest_best_streak_length, " "))+" |\n"
                 formatted_line += "-".center(length, "-")+"\n"
-                print(formatted_line)
+                print(formatted_line)               
